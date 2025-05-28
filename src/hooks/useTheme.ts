389263cceee5139bb794a851d77a89b9714a436e
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-28 15:10:35 +0530

    feature: Create 'useTheme' custom theme to detect dark mode

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,59 @@
+import { useCallback, useEffect, useState } from 'react';
+
+type Theme = 'light' | 'dark';
+
+export function useTheme() {
+    const [isDarkMode, setIsDarkMode] = useState<boolean>(
+        Boolean(getUserPreferredTheme()?.matches)
+    );
+
+    const changeTheme = useCallback((newTheme: Theme) => {
+        switch (newTheme) {
+            case 'light':
+                setIsDarkMode(false);
+                break;
+            case 'dark':
+                setIsDarkMode(true);
+                break;
+        }
+    }, []);
+
+    const toggleTheme = useCallback(() => {
+        setIsDarkMode((prevIsDarkMode) => !prevIsDarkMode);
+    }, []);
+
+    useEffect(() => {
+        const ctrl = new AbortController();
+
+        const darkModeQuery = getUserPreferredTheme();
+        if (darkModeQuery) {
+            darkModeQuery.addEventListener('change', () => {
+                if (getUserPreferredTheme()?.matches) {
+                    setIsDarkMode(true);
+                } else {
+                    setIsDarkMode(false);
+                }
+            });
+        }
+
+        return () => ctrl.abort();
+    }, []);
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.body.classList.add('dark');
+        } else {
+            document.body.classList.remove('dark');
+        }
+    }, [isDarkMode]);
+
+    return { isDarkMode, changeTheme, toggleTheme };
+}
+
+function getUserPreferredTheme() {
+    if (window.matchMedia) {
+        return window.matchMedia('(prefers-color-scheme: dark)');
+    }
+
+    return null;
+}
