10ab0179592186ea4c76b969c0edf48f4f3a0eb6
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-28 14:40:44 +0530

    feature: Setup the shadcn and install Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
-    return <div>Hello, World</div>;
+    return (
+        <div>
+            <Button>Button</Button>
+        </div>
+    );
 }
 
 export default App;
