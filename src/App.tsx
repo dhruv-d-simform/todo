97824085a64bc86b22b65907fa3404fba77f24b0
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-28 16:41:46 +0530

    feature: Implement the Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Button } from '@/components/ui/button';
-import { useThemeContext } from '@/theme/ThemeContext';
+import { Header } from '@/components/Header';
 
 function App() {
-    const { isDarkMode, toggleTheme } = useThemeContext();
     return (
         <div>
-            <Button onClick={toggleTheme}>Toggle Theme</Button>
-
-            <p>{isDarkMode ? 'DARK' : 'LIGHT'}</p>
+            <Header />
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,59 @@
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+
+import { useThemeContext } from '@/theme/ThemeContext';
+
+import lightModeIcon from '/icons/light_mode.svg';
+import darkModeIcon from '/icons/dark_mode.svg';
+import searchIcon from '/icons/search.svg';
+
+export function Header() {
+    const { isDarkMode, toggleTheme } = useThemeContext();
+
+    return (
+        <header className="w-full mx-auto h-16 border-b flex justify-between items-center gap-6 px-6">
+            <a href="#" className="text-2xl font-bold text-nowrap">
+                <span className="text-main">To</span>-Do
+            </a>
+
+            <div className="w-full max-w-[30rem] relative">
+                <Input
+                    type="text"
+                    placeholder="Search your task here..."
+                    className="w-full h-10 pr-12"
+                />
+
+                <Button className="absolute top-0 right-0 aspect-square h-full p-1.5 cursor-pointer bg-main hover:bg-main hover:opacity-90 active:opacity-80">
+                    <img src={searchIcon} className="w-full h-full" />
+                </Button>
+            </div>
+
+            <div className="flex justify-center items-center gap-4">
+                <p className="flex flex-col justify-center items-center gap-1">
+                    <span className="text-[14px]/[14px]">
+                        {new Date().toLocaleString('en-in', {
+                            weekday: 'long',
+                        })}
+                    </span>
+                    <span className="text-[#3ABEFF] text-[14px]/[14px]">
+                        {new Date().toLocaleString('en-in', {
+                            dateStyle: 'short',
+                        })}
+                    </span>
+                </p>
+
+                <Button
+                    variant="outline"
+                    onClick={toggleTheme}
+                    className="cursor-pointer p-2.5 w-11 h-11"
+                >
+                    <img
+                        src={isDarkMode ? lightModeIcon : darkModeIcon}
+                        alt=""
+                        className="w-full h-full"
+                    />
+                </Button>
+            </div>
+        </header>
+    );
+}
