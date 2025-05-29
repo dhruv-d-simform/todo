8f9a9be5777812466d412a01dc31cdc543b52e24
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-29 10:52:16 +0530

    feature: Add icons to used in nav items of sidebar

---
+++ src/assets/UpcomingIcon.tsx
@@ -0,0 +1,23 @@
+export function UpcomingIcon({ style }: { style?: React.CSSProperties }) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            stroke="currentColor"
+            style={style ?? { width: '1.5rem', height: '1.5rem' }}
+        >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+                <g>
+                    <path d="M17 3h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4V1h2v2h6V1h2v2zm3 6V5h-3v2h-2V5H9v2H7V5H4v4h16zm0 2H4v8h16v-8zM6 13h5v4H6v-4z"></path>{' '}
+                </g>
+            </g>
+        </svg>
+    );
+}
