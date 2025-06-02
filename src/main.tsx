2a50cf68b85c1c23846be0f2548a542906e09667
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-02 14:40:43 +0530

    feature: Install and setup redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from '@/theme/ThemeProvider';
-import { router } from './router';
+import { router } from '@/router';
+import { store } from '@/redux/store';
 import '@/index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider>
-            <RouterProvider router={router} />
+            <Provider store={store}>
+                <RouterProvider router={router} />
+            </Provider>
         </ThemeProvider>
     </StrictMode>
 );
