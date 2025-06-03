57a670bd0f96637ea0c29be0aa8c35aa4aa1271c
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-03 16:41:58 +0530

    refractor: Remove unnecessary comments from todo.types.ts

--- src/types/todo.types.ts
+++ src/types/todo.types.ts
@@ -1,6 +1,5 @@
 export const PRIORITY_OPTIONS = ['high', 'medium', 'low'] as const;
 
-// export type Priority = 'low' | 'medium' | 'high';
 export type Priority = (typeof PRIORITY_OPTIONS)[number];
 
 export interface Todo {
