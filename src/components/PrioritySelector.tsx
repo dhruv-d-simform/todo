bfedcdce1087c27113746e3c14c405e58f831bce
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-03 16:33:10 +0530

    feature: Create PrioritySelector component

---
+++ src/components/PrioritySelector.tsx
@@ -0,0 +1,38 @@
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from './ui/select';
+import { PRIORITY_OPTIONS, type Priority } from '@/types/todo.types';
+
+interface PrioritySelectorProps {
+    selectedPriority: Priority;
+    onValueChange: (value: Priority) => void;
+}
+
+export function PrioritySelector({
+    selectedPriority,
+    onValueChange,
+}: PrioritySelectorProps) {
+    return (
+        <Select
+            value={selectedPriority}
+            onValueChange={(value) => {
+                onValueChange(value as Priority);
+            }}
+        >
+            <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select Priority" />
+            </SelectTrigger>
+            <SelectContent>
+                {PRIORITY_OPTIONS.map((option) => (
+                    <SelectItem key={option} value={option}>
+                        {option.charAt(0).toUpperCase() + option.slice(1)}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    );
+}
