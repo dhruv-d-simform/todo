00c1d2bbcb113cde61f3cf0243b0381bc2b01500
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-04 17:43:46 +0530

    feature: Create utility for todos to get colors

---
+++ src/utils/todoUtils.ts
@@ -0,0 +1,44 @@
+import { Priority } from '@/types/todo.types';
+import { format } from 'date-fns';
+
+export function getPriorityStyles(priority: Priority) {
+    switch (priority) {
+        case 'high':
+            return {
+                color: '#E23B37',
+                bgColor: '#F8DAD8',
+            };
+        case 'medium':
+            return {
+                color: '#2089E5',
+                bgColor: '#E0EDF7',
+            };
+        case 'low':
+            return {
+                color: '#FE6E01',
+                bgColor: '#FBEDE3',
+            };
+    }
+}
+
+export function getCalendarLabelAndColor(date: string | undefined) {
+    const today = format(new Date(), 'yyyy-MM-dd');
+    const tomorrow = format(new Date(Date.now() + 86400000), 'yyyy-MM-dd');
+
+    if (date === today) {
+        return {
+            label: 'Today',
+            color: '#00b330',
+        };
+    } else if (date === tomorrow) {
+        return {
+            label: 'Tomorrow',
+            color: '#db7c00',
+        };
+    } else {
+        return {
+            label: 'Upcoming',
+            color: '#975dee',
+        };
+    }
+}
