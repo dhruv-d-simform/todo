59b3d05f4f4170c9bb660cf8b23d13e13cf903ac
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-04 15:20:16 +0530

    feature: Add validation to prevent only number in title

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -2,7 +2,14 @@ import { z } from 'zod';
 import { PRIORITY_OPTIONS } from '@/types/todo.types';
 
 export const todoFormSchema = z.object({
-    title: z.string().trim().nonempty('Title cannot be empty.'),
+    title: z
+        .string()
+        .trim()
+        .nonempty('Title cannot be empty.')
+        .refine(
+            (title) => !isFinite(Number(title)),
+            'Title cannot be only number.'
+        ),
     description: z.string().optional(),
     date: z.date().optional(),
     priority: z.enum(PRIORITY_OPTIONS),
