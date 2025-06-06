b266ffa837281c6fd85ec527a8f2348b83547d13
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-06 10:39:23 +0530

    refractor: Create a separate function to check date filter in filterTodos.ts

--- src/utils/filterTodos.ts
+++ src/utils/filterTodos.ts
@@ -1,21 +1,28 @@
 import { format } from 'date-fns';
 import type { Todo } from '@/types/todo.types';
 
+type DateOptions = 'today' | 'upcoming';
+
+function filterTodoByDate(todo: Todo, date?: DateOptions) {
+    const today = format(new Date(), 'yyyy-MM-dd');
+    if (date === 'today') {
+        if (todo.date !== today) return true;
+    } else if (date === 'upcoming') {
+        if (!todo.date || todo.date === today) return true;
+    }
+
+    return false;
+}
+
 export function filterTodos(
     todos: Array<Todo>,
     filter: string,
-    date?: 'today' | 'upcoming'
+    date?: DateOptions
 ): Array<Todo> {
-    const today = format(new Date(), 'yyyy-MM-dd');
-
     filter = filter.toLowerCase();
 
     return todos.filter((todo) => {
-        if (date === 'today') {
-            if (todo.date !== today) return false;
-        } else if (date === 'upcoming') {
-            if (!todo.date || todo.date === today) return false;
-        }
+        if (filterTodoByDate(todo, date)) return false;
 
         if (todo.title.toLowerCase().includes(filter)) return true;
         if (todo.description?.toLowerCase().includes(filter)) return true;
